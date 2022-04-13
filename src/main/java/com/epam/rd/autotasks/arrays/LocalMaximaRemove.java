f6483898146ac06f27f09d94f184af51a30470d3
Beata Podgorska <beata.podgorska26@gmail.com>
2022-04-13 20:28:23 +0200

    Solved

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -13,7 +13,41 @@ public class LocalMaximaRemove {
     public static int[] removeLocalMaxima(int[] array){
 
         //put your code here
+        int[] copyOfArray = new int[array.length];
+        int counter = 0; //counter of copyOfArray index 
 
-        throw new UnsupportedOperationException();
+        for(int i = 0; i< array.length;i++){  
+            
+            //if the value is not a maxima add to the copy of array
+            if(i==0){
+                if(array[i] <= array[i+1]){
+                    copyOfArray[counter] = array[i];
+                    counter++;
+                }
+            }
+            else if(i == (array.length-1)){
+                if(array[i] <= array[i-1]){
+                    copyOfArray[counter] = array[i];
+                    counter++;
+                }
+            }
+            else{
+                if(array[i]<=array[i+1] || array[i]<=array[i-1]){
+                    copyOfArray[counter] = array[i];
+                    counter++;
+                }
+            }
+        }
+
+        int[] withoutMaxima = new int[counter];
+
+        //adding elements to the final array
+        for(int i =0; i<counter;i++){
+            withoutMaxima[i] = copyOfArray[i];
+        }
+
+        return withoutMaxima;
+
+        //throw new UnsupportedOperationException();
     }
 }
